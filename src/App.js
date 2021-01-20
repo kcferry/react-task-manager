bf8308e2ade7d982ada82dc33bcdfabae2807582
Kyle <kyleferry@icloud.com>
2021-01-20 06:26:27 +0000

    Added tasks components & state & delete tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+
+  const [tasks, settasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2.30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Meeting',
+        day: 'Feb 6th at 1.30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2.30pm',
+        reminder: false
+    },
+  ])
+
+  // Delete Task
+
+  const deleteTask = (id) => {
+    settasks(tasks.filter((task) => task.id !== id ))
+  }
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
     </div>
   );
 }
+
+
 
 export default App; 
---
+++ src/components/Tasks.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import Task from './Task'
+
+
+const Tasks = ({tasks, onDelete}) => {
+
+    return (
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete}/>
+            )
+            )}
+        </>
+    )
+}
+
+Tasks.propTypes = {
+
+}
+
+export default Tasks
