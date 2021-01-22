06bd3a246ebcf35683c599847dabccd5889e83c7
Kyle <kyleferry@icloud.com>
2021-01-22 14:14:30 +0000

    Added react router & about page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 //import { v4 as uuidv4 } from 'uuid';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -83,22 +86,32 @@ function App() {
   }
 
   return (
+    <Router>
+
     <div className="container">
 
       <Header onAdd={() => setShowAddTask(!showAddTask)}
       showAdd={showAddTask} />
 
-      {showAddTask && <AddTask onAdd={addTask}/> }
+      <Route path='/' exact render={(props) => (
+        <>
+          { showAddTask && <AddTask onAdd={addTask}/> }
 
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} 
-        onDelete={deleteTask}
-        onToggle={toggleReminder}
-        /> 
-      ) : (
-          'No Tasks To Show')}
+          { tasks.length > 0 ? (
+            <Tasks tasks={tasks} 
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+            /> 
+          ) : (
+              'No Tasks To Show') }
+        </>
+      )} /> 
+      <Route path='/about' component={About} />
+      <Footer />
 
     </div>
+    
+    </Router>
   );
 }
 
